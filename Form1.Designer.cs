dc4f5bff2e091b3c0adea6883c16a94738ca7edc
Vitor Gaefke <vitorgaefke@gmail.com>
2022-11-04 16:19:54 -0300

    tela de login apenas msg de erro

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Livraria
 {
-    partial class Form1
+    partial class Livraria
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,21 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btLogin = new System.Windows.Forms.Button();
+            this.txtPswd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // btLogin
+            // 
+            this.btLogin.Location = new System.Drawing.Point(580, 310);
+            this.btLogin.Name = "btLogin";
+            this.btLogin.Size = new System.Drawing.Size(75, 23);
+            this.btLogin.TabIndex = 1;
+            this.btLogin.Text = "LOGAR!";
+            this.btLogin.UseVisualStyleBackColor = true;
+            this.btLogin.Click += new System.EventHandler(this.btLogin_Click);
+            // 
+            // txtPswd
+            // 
+            this.txtPswd.Location = new System.Drawing.Point(460, 249);
+            this.txtPswd.Name = "txtPswd";
+            this.txtPswd.Size = new System.Drawing.Size(195, 20);
+            this.txtPswd.TabIndex = 2;
+            this.txtPswd.Text = "SUA SENHA AQUI";
+            this.txtPswd.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(457, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "SENHA:";
+            // 
+            // txtUser
+            // 
+            this.txtUser.AccessibleDescription = "";
+            this.txtUser.AccessibleName = "";
+            this.txtUser.Location = new System.Drawing.Point(460, 170);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(195, 20);
+            this.txtUser.TabIndex = 0;
+            this.txtUser.Text = "SEU EMAIL AQUI";
+            this.txtUser.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(457, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "USUÁRIO:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(460, 310);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "CANCELAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Livraria
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPswd);
+            this.Controls.Add(this.btLogin);
+            this.Controls.Add(this.txtUser);
+            this.Name = "Livraria";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btLogin;
+        private System.Windows.Forms.TextBox txtPswd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,14 +10,56 @@ using System.Windows.Forms;
 
 namespace Livraria
 {
-    public partial class Form1 : Form
+    public partial class Livraria : Form
     {
-        public Form1()
+        public Livraria()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtUser.Text.Equals("vitorgaefke@gmail.com") && txtPswd.Text.Equals("12345"))
+                {
+                    //logar
+                } else
+                {
+                    MessageBox.Show("Usuário ou senha incorretos",
+                                    "DESCULPE",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Exclamation);
+                }
+            }
+
+            catch
+            {
+
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
